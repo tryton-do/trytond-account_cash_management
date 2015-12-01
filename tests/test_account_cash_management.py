a075a426ec927f497927989f3dde7592c75fd2ab
resteve <resteve@zikzakmedia.com>
2015-12-01 10:55:56 +0100

    Tests

--- tests/test_account_cash_management.py
+++ tests/test_account_cash_management.py
@@ -1,30 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-# This file is part of account_cash_management module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the account_cash_management module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountPaymentTestCase(unittest.TestCase):
-    'Test AccountPayment module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_cash_management')
-
-    def test0005views(self):
-        'Test views'
-        test_view('account_cash_management')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class AccountCashManagementTestCase(ModuleTestCase):
+    'Test Account Cash Management module'
+    module = 'account_cash_management'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        AccountPaymentTestCase))
+        AccountCashManagementTestCase))
     return suite
